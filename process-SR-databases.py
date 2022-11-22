cd00a93ade9b48b04d5af91c66dae83b9c9839c5
Máté Gyöngyösi <gyongyosimate1@gmail.com>
2022-11-22 15:03:46 +0100

    Second commit.

--- process-SR-databases.py
+++ process-SR-databases.py
@@ -1,70 +1,149 @@
+"""
+A program for **making** MÁV's internal, really **dirty database** of speed restrictions **computer-processable.**
+
+Receives:
+several _speed restriction databases_ in Excel formats;
+a .csv _database of stations,_ incl. their names and UIC codes;
+a .csv _database of UIC member railway companies,_ incl. their names and UIC codes;
+a .csv _database of UIC member countries,_ incl. their names, ISO and UIC codes.
+
+Returns a neat, computer-processable _.csv database of all speed restrictions_ with extended contents.
+
+Abbreviations:
+_SR = speed restriction_;
+_TSR = temporary speed restriction_;
+_ASR = all speed restrictions_.
+"""
+
 from openpyxl import load_workbook
 import pandas as pd
 from pandas import DataFrame
 import re
 import roman
+import numpy as np
+from natsort import index_realsorted
 
 
-def import_worksheets(excel_file_location):
+def import_worksheets(excel_file_location, openpyxl_db):
+    """
+    Receives an _Excel file location_ and a _database_.
+
+    Imports an _Excel file_ using `openpyxl`.
+    Appends _all worksheets_ from the _Excel file_ to the _database_.
+
+    Returns the _database_.
+    """
     excel_workbook = load_workbook(excel_file_location)
     print(excel_file_location + ' loaded!')
+
     for excel_worksheet in excel_workbook.worksheets:
-        openpyxl_db_outer.append(excel_worksheet)
+        openpyxl_db.append(excel_worksheet)
     print('All worksheets imported from' + excel_file_location + ' to openpyxl_db!')
-    return openpyxl_db_outer
+    return openpyxl_db
 
 
 def check_permanence(cell):
+    """
+    Receives an Excel file's _cell_ imported via `openpyxl`.
+    Returns _if that cell is bold_ using `openpyxl`.
+    """
     return cell.font.bold
 
 
 def convert_cells_to_values(row):
+    """
+    Receives _cells_ of an Excel file's row imported via `openpyxl`.
+    Returns the same row but with just _each cell's values_.
+    """
     for idx_02, cell in enumerate(row):
         row[idx_02] = cell.value
 
 
 def import_csv(file_location, index_col_loc):
-    print(file_location + ' imported!')
+    """
+    Receives a .csv file's location and the index column's number.
+    Returns the .csv file's content as a `pandas` `DataFrame`.
+    """
+    print(file_location + ' will now be imported!')
     return pd.read_csv(file_location, sep=';', index_col=index_col_loc, dtype=str)
 
 
 def lookup_data(database, old_data, column_name):
+    """
+    Receives a _text,_ a _database,_ and a _column name_ to look for.
+    Returns the _text_ in the same row's searched column where the input text was _found._
+    Raises an error if searched data could not be found.
+    """
     try:
-        new_data = str(database.at[old_data, column_name])
+        return str(database.at[old_data, column_name])
     except KeyError:
         print('Error: ' + old_data + ' could not be converted to ' + column_name + '!')
-        new_data = None
-    return new_data
+        return None
 
 
 def extract_number(text):
+    """
+    Receives a text.
+
+    Initializes *regex search expressions* for arabic and roman numbers with several combinations
+    that could be found in the database.
+
+    Searches for an _arabic number._
+    If found, returns it.
+    If not found, searches for a _roman number._
+
+    If found, converts it to an _arabic number_ via `roman` and returns it.
+    If this doesn't succeed, returns an `InvalidRomanNumeralError`.
+    If not found, searches for an _arabic number with letters._
+
+    If found, returns it.
+    If not found, returns a `TypeError`.
+    """
     arabic_number_regex = r'((?:^)|(?<= |\.|\())' \
                           r'\d+' \
                           r'(?=\D)'
     find_roman_number_regex = r'((?:^)|(?<= |\.|\())' \
                               r'((M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3}))' \
-                              r'|(M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3}))/[a-z]' \
+                              r'|(M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3}))\/[a-z]' \
                               r'|(M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3}))[a-z])' \
                               r'(?=\.| )'
     get_roman_number_regex = r'M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})'
     letter_and_arabic_number_regex = r'((?:^)|(?<= |\.|\())\w\d+(?=\D)'
+
+    # TODO: InvalidRomanNumeralError:
+    #  "Chinoin" vontató vágány és a kapcsolódó
+    #  VII/a. sz. vágány
+    #  XIVa.vg.
+    #  B II/a. vágány
+
+    # TODO: TypeError:
+    #  "T" vágány
+    #  AUDI kihúzóvágány
+    #  Duna vontatóvágány
+    #  "Amerikai" kihúzó vágány
+    #  "Újkezelő" vágány
+    #  Józsefvárosi összekötő vágány
+    #  "Házi" csonka vágány
+    #  Homlokrakodó csonka vágány
+    #  MOL kihúzó
+    #  Fűtőház vágány
+
     try:
         if re.search(arabic_number_regex, text, re.MULTILINE) is not None:
-            arabic_number = re.search(arabic_number_regex, text, re.MULTILINE)[0]
+            return re.search(arabic_number_regex, text, re.MULTILINE)[0]
         else:
             if re.search(find_roman_number_regex, text, re.MULTILINE) is not None:
                 if re.search(find_roman_number_regex, text, re.MULTILINE)[0] != '':
-                    roman_number = re.search(get_roman_number_regex, text, re.MULTILINE)[0]
-                    arabic_number = roman.fromRoman(roman_number)
+                    roman_number = re.search(find_roman_number_regex, text, re.MULTILINE)[0]
+                    return roman.fromRoman(roman_number)
                 else:
-                    arabic_number = re.search(letter_and_arabic_number_regex, text, re.MULTILINE)[0]
+                    return re.search(letter_and_arabic_number_regex, text, re.MULTILINE)[0]
             else:
-                arabic_number = re.search(letter_and_arabic_number_regex, text, re.MULTILINE)[0]
+                return re.search(letter_and_arabic_number_regex, text, re.MULTILINE)[0]
     except TypeError:
-        arabic_number = 'TypeError'
+        return 'TypeError'
     except roman.InvalidRomanNumeralError:
-        arabic_number = 'InvalidRomanNumeralError'
-    return arabic_number
+        return 'InvalidRomanNumeralError'
 
 
 def add_data(row,
@@ -73,6 +152,18 @@ def add_data(row,
              country_uic_database,
              company_uic_database,
              station_database):
+    """
+    Receives a _SR's row,_
+    a _database_,
+    if the SR is _in the timetables,_
+    the .csv _database of UIC member countries,_
+    the .csv _database of UIC member railway companies,_
+    the .csv _database of stations,_.
+
+    Processes all data in the row.
+
+    Appends the _processed row_ to the _database._
+    """
     convert_cells_to_values(row)
 
     country_iso = 'HU'
@@ -88,21 +179,34 @@ def add_data(row,
     row[3:3] = [company_uic]
 
     row[5] = re.sub(r'(?<=\w)- (?=\w)', '-', str(row[5]))
-    from_station_uic = lookup_data(station_database, row[5], 'statisztikai szám')
-    row[6:6] = [from_station_uic]
+
+    try:
+        row[6:6] = re.search(r'(?<=HU).*',
+                             lookup_data(station_database, row[5], 'PLC kód'))[0]
+    except TypeError:
+        row[6:6] = 'TypeError'
 
     on_station = not row[7]
     station_to_uic = track_station = switch_station = None
     if on_station is False:
         row[7] = re.sub(r'(?<=\w)- (?=\w)', '-', str(row[7]))
-        station_to_uic = lookup_data(station_database, row[7], 'statisztikai szám')
+        try:
+            station_to_uic = re.search(r'(?<=HU).*',
+                                       lookup_data(station_database, row[7], 'PLC kód'))[0]
+        except TypeError:
+            station_to_uic = 'TypeError'
         sr_applied_to = 'track'
         if row[8] == 'bal vágány':
             row[8] = 'left'
         elif row[8] == 'jobb vágány':
             row[8] = 'right'
+        elif row[8] is None:
+            row[8] = 'single'
         else:
             row[8] = 'error'
+
+        # TODO: save number of tracks to separate column
+
     else:
         row[8] = None
         if str(row[9]).find('kitérő') != -1:
@@ -112,6 +216,11 @@ def add_data(row,
             sr_applied_to = 'track'
             track_station = extract_number(str(row[9]))
 
+    # TODO: save roman numerals as well
+    # TODO: track's UIC code
+    # TODO: SR's direction
+    # TODO: SR's permanence
+
     row[8:8] = [station_to_uic]
     row[9:9] = [on_station]
     row[10:10] = [sr_applied_to]
@@ -119,6 +228,8 @@ def add_data(row,
     row[13:13] = [track_station]
     row[14:14] = [switch_station]
     row[15:15] = [in_timetable]
+
+    # TODO: megszüntetés (tervezett) dátuma
 
     pre_df_db.append(row)
 
@@ -129,9 +240,12 @@ def create_database(openpyxl_db,
                     country_uic_database_file_location,
                     company_uic_database_file_location,
                     station_database_file_location):
+    """
+    Processes SRs via `add_data` and adds them to the list database.
+    """
     country_uic_database = import_csv(country_uic_database_file_location, 1)
     company_uic_database = import_csv(company_uic_database_file_location, 1)
-    station_database = import_csv(station_database_file_location, 1)
+    station_database = import_csv(station_database_file_location, 0)
     for excel_worksheet in openpyxl_db:
         # get rows after the header
         for row in excel_worksheet.iter_rows(min_row=2):
@@ -146,19 +260,24 @@ def create_database(openpyxl_db,
                          station_database)
 
         # delete footer row
-        del pre_df_db[-1]
+
     print('All rows added to pre_df_db!')
     return pre_df_db
 
 
-def create_dataframe(pre_df_db):
-    full_df = DataFrame(pre_df_db)
-    print('pre_df_db converted to dataframe!')
-    return full_df
+def create_dataframe(database):
+    """
+    Creates a `DataFrame` from a list database.
+    """
+    print('database will now be converted to a DataFrame!')
+    return DataFrame(database)
 
 
 def rename_columns(full_df):
-    column_names = ['country_ISO',
+    """
+    Renames columns in a `DataFrame` to match updated content.
+    """
+    full_df.columns = ['country_ISO',
                     'country_UIC',
                     'company_name',
                     'company_UIC',
@@ -184,60 +303,92 @@ def rename_columns(full_df):
                     'cause',
                     'time_to',
                     'comment']
-    full_df.columns = column_names
-    print('Columns renamed!')
-    return full_df
+    print('full_df\'s columns renamed!')
 
 
 def drop_columns(full_df):
-    full_df = full_df.drop(columns=['length',
-                                    'length_time'])
+    """
+    Drops unnecessary columns from a `DataFrame`.
+    """
+    full_df.drop(columns=['length',
+                          'length_time'])
     print('Unnecessary columns dropped from full_df!')
-    return full_df
 
 
 def edit_full_df(full_df):
-    full_df = rename_columns(full_df)
-    full_df = drop_columns(full_df)
+    """
+    Calls `rename_columns` and `drop_columns`.
+
+    Removes leftover rows from the Excel import.
+
+    Sorts the database.
+    """
+    rename_columns(full_df)
+    drop_columns(full_df)
+    full_df.query("line != 'Vonal'", inplace=True)
+    full_df.query("location_from != 'Hossz (m):'", inplace=True)
+    full_df['line'] = full_df['line'].astype(str)
+    full_df.sort_values(by=['line', 'location_from'],
+                        key=lambda x: np.argsort(
+                            index_realsorted(
+                                zip(full_df['line'],
+                                    full_df['location_from']))),
+                        inplace=True)
     print('full_df edited!')
-    return full_df
 
 
 def save_file(full_df, save_path):
+    """
+    Saves the input `DataFrame` in .csv to the input path.
+    """
     full_df.to_csv(save_path, index=False)
     print('full_df saved to ' + save_path + '!')
 
 
-folder_location = '/Users/gymate1/Library/Mobile Documents/com~apple~CloudDocs/Menetirány/lassújelek'
-TSR_excel_file_location = folder_location + '/01_source/data/SR_lists/MÁV_220202_TSR.xlsx'
-ASR_excel_file_location = folder_location + '/01_source/data/SR_lists/MÁV_220202_ASR.xlsx'
-SR_file_locations = [TSR_excel_file_location, ASR_excel_file_location]
-pre_df_db_outer = []
-country_uic_database_file_location_outer = folder_location + '/01_source/data/UIC_lists/country_UIC_list.csv'
-company_uic_database_file_location_outer = folder_location + '/01_source/data/UIC_lists/company_UIC_list.csv'
-station_database_file_location_outer = folder_location + '/01_source/data/railway-line_lists/railway-lines_HU.csv'
-print('Variables initialized!')
+def main():
+    """
+    Initializes external database locations.
 
-for idx, file_location_outer in enumerate(SR_file_locations):
-    openpyxl_db_outer = []
-    openpyxl_db_outer = import_worksheets(SR_file_locations[idx])
-    if idx == 0:
-        import_all_outer = True
-    else:
-        import_all_outer = False
-    pre_df_db_outer = create_database(openpyxl_db_outer,
-                                      pre_df_db_outer,
-                                      import_all_outer,
-                                      country_uic_database_file_location_outer,
-                                      company_uic_database_file_location_outer,
-                                      station_database_file_location_outer)
+    Imports SR databases via `import_worksheets`.
+    Creates a list database from each file via `create_database`,
+    taking in account if all SRs should be imported from them.
+    Converts the list database to a dataframe via `create_dataframe`.
+    Edits the dataframe via `edit_full_df`.
+    Saves the dataframe via `save_file`.
+    """
+    folder_location = '/Users/gymate1/Library/Mobile Documents/com~apple~CloudDocs/Menetirány/lassújelek'
+    tsr_excel_file_location = folder_location + '/01_source/data/SR_lists/MÁV_220202_TSR.xlsx'
+    asr_excel_file_location = folder_location + '/01_source/data/SR_lists/MÁV_220202_ASR.xlsx'
+    sr_file_locations = [tsr_excel_file_location, asr_excel_file_location]
+    pre_df_db = []
+    country_uic_database_file_location = folder_location + '/01_source/data/UIC_lists/country_UIC_list.csv'
+    company_uic_database_file_location = folder_location + '/01_source/data/UIC_lists/company_UIC_list.csv'
+    station_database_file_location = folder_location + '/01_source/data/station_lists/stations_HU.csv'
+    print('Variables initialized!')
 
-full_df_outer = create_dataframe(pre_df_db_outer)
-full_df_outer = edit_full_df(full_df_outer)
-print('full_df done!')
+    for i, file_location in enumerate(sr_file_locations):
+        openpyxl_db = []
+        openpyxl_db = import_worksheets(file_location, openpyxl_db)
+        if i == 0:
+            import_all = True
+        else:
+            import_all = False
+        pre_df_db = create_database(openpyxl_db,
+                                    pre_df_db,
+                                    import_all,
+                                    country_uic_database_file_location,
+                                    company_uic_database_file_location,
+                                    station_database_file_location)
 
-new_file_name = '/03_export/_all_220202_ASR.csv'
-save_path_outer = folder_location + new_file_name
-save_file(full_df_outer, save_path_outer)
+    full_df = create_dataframe(pre_df_db)
+    edit_full_df(full_df)
+    print('full_df done!')
 
-print('All done!')
+    new_file_name = '/03_export/_all_220202_ASR.csv'
+    save_path = folder_location + new_file_name
+    save_file(full_df, save_path)
+
+    print('All done!')
+
+
+main()
