8fdc4e5c6d4ebd91d286f93e2ba550bebf0ed455
Máté Gyöngyösi <gyongyosimate1@gmail.com>
2023-12-29 18:47:23 +0100

    Logger integrated, database connection extracted

--- src/process_new_email/main.py
+++ src/process_new_email/main.py
@@ -1,25 +1,39 @@
+import logging
+
 from dotenv import load_dotenv
 
+from src.process_new_email.database_connection import DatabaseServer
 from src.process_new_email.table_updaters.country_codes import CountryCodesUpdater
 
 
-def main():
-    load_dotenv()
-    
-    update_country_codes('https://uic.org/spip.php?action=telecharger&arg=')
-    
-    
-def update_country_codes(uic_root_url: str) -> None:
-    country_codes_updater = CountryCodesUpdater(
-        f'{uic_root_url}322',
-        f'{uic_root_url}320'
+def main() -> None:
+    logging.basicConfig(
+        encoding="utf-8",
+        handlers=[logging.StreamHandler(),
+                  logging.FileHandler("kalauz.log")],
+        format="%(asctime)s – %(levelname)s @ %(name)s @ %(funcName)s: %(message)s",
+        level=logging.DEBUG,
     )
     
+    load_dotenv()
+
+    database_connection = DatabaseServer("kalauz")
+
+    update_country_codes(
+        database_connection, "https://uic.org/spip.php?action=telecharger&arg="
+    )
+
+
+def update_country_codes(database_connection, uic_root_url: str) -> None:
+    country_codes_updater = CountryCodesUpdater(
+        database_connection, f"{uic_root_url}322", f"{uic_root_url}320"
+    )
+
     country_codes_updater.process_data()
     country_codes_updater.store_data()
-    
+
     country_codes_updater.logger.info("Table `country_codes` sucessfully updated!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- src/process_new_email/table_updaters/common.py
+++ src/process_new_email/table_updaters/common.py
@@ -1,43 +1,34 @@
 from abc import ABC, abstractmethod
 from io import BytesIO
-import os
+import logging
 from typing import Final
 
-import mysql.connector
 import requests
 
-from src.logger import LoggerMixin
+from src.process_new_email.database_connection import DatabaseServer
 
 
-class HelperTableUpdater(LoggerMixin, ABC):
+class HelperTableUpdater(ABC):
+    def __init__(self, database_connection: DatabaseServer, data_url: str) -> None:
+        self.logger = logging.getLogger(__name__)
 
-    def __init__(self, data_url: str):
-        super().__init__()
-        
         self._DATA_URL: Final = data_url
         self._DATA_TO_PROCESS: Final[BytesIO] = self.download_data(data_url)
-        
-        self.CONNECTION_TO_DATABASE: Final = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password=os.getenv("DATABASE_PASSWORD"),
-            database="kalauz"
-        )
-        self.CURSOR: Final = self.CONNECTION_TO_DATABASE.cursor()
-    
+
+        self.DATABASE: Final = database_connection
+
     def download_data(self, url: str) -> BytesIO:
         response = requests.get(
             url,
             headers={
-                "User-Agent":
-                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
-                    "AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.1.2 Safari/605.1.15"
-            }
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
+                "AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.1.2 Safari/605.1.15"
+            },
         )
         response.raise_for_status()
         self.logger.info(f"File successfully downloaded from {url}!")
         return BytesIO(response.content)
-    
+
     @abstractmethod
     def process_data(self) -> None:
         pass
