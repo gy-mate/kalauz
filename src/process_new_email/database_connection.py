3e92e571322318080db4c6882f6cb4375f27ab97
Máté Gyöngyösi <gyongyosimate1@gmail.com>
2023-12-30 20:44:37 +0100

    Starting replacement of direct database connection

--- src/process_new_email/database_connection.py
+++ src/process_new_email/database_connection.py
@@ -4,29 +4,31 @@ import os
 import mysql.connector
 from mysql.connector import CMySQLConnection, MySQLConnection
 from mysql.connector.pooling import PooledMySQLConnection
+from sqlalchemy import Engine, create_engine, URL
 
 
 class DatabaseServer:
     def __init__(self, database_name: str) -> None:
         self.logger = logging.getLogger(__name__)
 
-        self.connection = self._connect_to_server()
-        self.cursor = self.connection.cursor()
+        self.database_engine = self._connect_to_database_server()
 
         self._connect_to_database(database_name)
 
         self.logger.info(f"{self.__class__.__name__} initialized!")
 
-    def _connect_to_server(
+    def _connect_to_database_server(
         self,
-    ) -> PooledMySQLConnection | MySQLConnection | CMySQLConnection:
+    ) -> Engine:
+        database_url = URL.create(
+            drivername="mysql+mysql-connector-python",
+            host="localhost",
+            port=3306,
+            username="root",
+            password=os.getenv("DATABASE_PASSWORD"),
+        )
         try:
-            return mysql.connector.connect(
-                host="localhost", user="root", password=os.getenv("DATABASE_PASSWORD")
-            )
-        except mysql.connector.errors.ProgrammingError:
-            self.logger.critical("Cannot connect to the database server!")
-            raise
+            return create_engine(database_url)
         finally:
             self.logger.info("Successfully connected to the database server!")
 
@@ -36,19 +38,19 @@ class DatabaseServer:
 
         if database_name not in existing_databases:
             self._create_database(database_name)
-        self.connection.close()
+        self.database_engine.close()
 
         self._connect_to_created_database(database_name)
 
     def _create_database(self, database_name: str) -> None:
         self.cursor.execute("create database " + database_name)
-        self.connection.commit()
+        self.database_engine.commit()
 
     def _connect_to_created_database(self, database_name: str) -> None:
-        self.connection = mysql.connector.connect(
+        self.database_engine = mysql.connector.connect(
             host="localhost",
             user="root",
             password=os.getenv("DATABASE_PASSWORD"),
             database=database_name,
         )
-        self.cursor = self.connection.cursor()
+        self.cursor = self.database_engine.cursor()
