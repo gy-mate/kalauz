496ca24e76e7c19f9b25b1d215334a23ff3e4ec1
Máté Gyöngyösi <gyongyosimate1@gmail.com>
2023-12-31 04:56:02 +0100

    Switched to dataframe and sqlalchemy

--- src/process_new_email/table_updaters/country_codes.py
+++ src/process_new_email/table_updaters/country_codes.py
@@ -1,5 +1,4 @@
 from abc import ABC
-import contextlib
 from io import BytesIO
 from typing import Final, final
 
@@ -7,7 +6,10 @@ from lxml import etree
 
 # noinspection PyProtectedMember
 from lxml.etree import _Element
+from pandas import DataFrame
+import pandas as pd
 from requests import HTTPError
+from sqlalchemy import text
 
 # TODO: remove comment in the line below when mypy library stubs are created for the module imported
 from varname import nameof  # type: ignore
@@ -16,46 +18,38 @@ import zipfile
 from src.process_new_email.table_updaters.common import HelperTableUpdater
 
 
-def _uic_code_not_assigned(values: list[str]) -> bool:
+def _uic_code_not_assigned(values: tuple[str, str, str]) -> bool:
     return values[1] is None
 
 
-def _correct_swapped_country_names(values: list[str]) -> None:
-    if values[2] and values[3] and values[4]:
-        values[2], values[3], values[4] = _swap_strings_at_comma(values[2:5])
-
-
-def _swap_strings_at_comma(strings: list[str]) -> list[str]:
-    swapped_strings = []
-    for s in strings:
-        parts = s.split(", ")
-        swapped_strings.append(" ".join(parts[::-1]))
-    return swapped_strings
+def _swap_name(name: str) -> str:
+    return " ".join(name.split(", ")[::-1])
 
 
 @final
 class CountryCodesUpdater(HelperTableUpdater, ABC):
-    def __init__(self, database_connection, data_url: str, xsd_url: str) -> None:
-        super().__init__(database_connection, data_url)
+    def __init__(self, database, data_url, xsd_url: str) -> None:
+        super().__init__(database, data_url)
 
-        self.data: _Element = _Element()
+        self._data_to_validate: _Element = _Element()
+        self.data: DataFrame | None = None
         self.namespace: dict | None = None
 
         self._TAG_ROW: Final = "ns1:Country"
         self._PATH_ROW: Final = f".//{self._TAG_ROW}"
         self._TAG_BEGINNING_COLUMN: Final = f"{self._TAG_ROW}_"
 
-        self._XSD_URL: Final = xsd_url
+        self.XSD_URL: Final = xsd_url
         try:
-            self.XSD_TO_PROCESS: Final[BytesIO] = super().download_data(xsd_url)
-            self._XSD: Final[etree.XMLSchema] = self._process_xsd()
+            self.xsd_to_process: Final[BytesIO] = super().download_data(xsd_url)
+            self._xsd: Final[etree.XMLSchema] = self._process_xsd()
         except (HTTPError, IndexError) as exception:
             self.logger.warning(exception)
 
         self.logger.info(f"{self.__class__.__name__} initialized!")
 
     def _process_xsd(self) -> etree.XMLSchema:
-        xsd_unzipped = self._unzip(self.XSD_TO_PROCESS)
+        xsd_unzipped = self._unzip(self.xsd_to_process)
         return etree.XMLSchema(etree.parse(xsd_unzipped))
 
     def _unzip(self, xsd: BytesIO) -> BytesIO:
@@ -63,19 +57,18 @@ class CountryCodesUpdater(HelperTableUpdater, ABC):
             file_names = zipped_file.infolist()
             if len(file_names) > 1:
                 raise IndexError(
-                    f"The .zip file downloaded from {self._XSD_URL} has more than one file in it!"
+                    f"The .zip file downloaded from {self.XSD_URL} has more than one file in it!"
                 )
             only_file_name = file_names[0]
             return BytesIO(zipped_file.read(only_file_name))
 
     def process_data(self) -> None:
-        downloaded_data = self._DATA_TO_PROCESS
-        self.data = etree.parse(downloaded_data).getroot()
+        self._data_to_validate = etree.parse(self._data_to_process).getroot()
         self.logger.info(
-            f"Data downloaded from {self._DATA_URL} successfully processed!"
+            f"Data downloaded from {self.DATA_URL} successfully processed!"
         )
 
-        self.namespace = self.data.nsmap
+        self.namespace = self._data_to_validate.nsmap
 
         try:
             self._validate_data()
@@ -84,13 +77,13 @@ class CountryCodesUpdater(HelperTableUpdater, ABC):
             raise
 
     def _validate_data(self) -> None:
-        if self._XSD and self._XSD.validate(self.data) is False:
+        if self._xsd and self._xsd.validate(self._data_to_validate) is False:
             raise ValueError(
-                f"The .xml file downloaded from {self._DATA_URL} is invalid "
-                f"according to the .xsd downloaded and unzipped from {self._XSD_URL}!"
+                f"The .xml file downloaded from {self.DATA_URL} is invalid "
+                f"according to the .xsd downloaded and unzipped from {self.XSD_URL}!"
             )
         self.logger.info(
-            f"Data downloaded from {self._DATA_URL} successfully validated!"
+            f"Data downloaded from {self.DATA_URL} successfully validated!"
         )
 
     def store_data(self) -> None:
@@ -108,60 +101,69 @@ class CountryCodesUpdater(HelperTableUpdater, ABC):
             primary key (UIC_code)
         )
         """
-        self.database.cursor.execute(query)
-        self.database.connection.commit()
+        with self.database.engine.begin() as connection:
+            connection.execute(text(query))
         self.logger.info("Table `countries` sucessfully created (if needed)!")
 
     def _add_data(self) -> None:
-        query = """
-        insert ignore into countries (
-            ISO_code,
-            UIC_code,
-            name_EN,
-            name_FR,
-            name_DE
+        # TODO: report wrong documentation URL of pd.read_xml() to JetBrains or pandas developers
+        self.data = pd.read_xml(
+            path_or_buffer=self._data_to_process,
+            xpath=self._PATH_ROW,
+            namespaces=self.namespace,
         )
-        values (%s, %s, %s, %s, %s)
-        """
-        for country in self.data.findall(self._TAG_ROW, namespaces=self.namespace):
-            values = list(self._extract_info(country))
-
-            if _uic_code_not_assigned(values):
-                continue
-            _correct_swapped_country_names(values)
-
-            self.database.cursor.execute(query, values)
-        self.database.connection.commit()
-        self.logger.info(
-            f"Successfully added new data downloaded from {self._DATA_URL} to table `countries`!"
+        self.data.rename(
+            columns={
+                "Country_ISO_Code": "ISO_code",
+                "Country_UIC_Code": "UIC_code",
+                "Country_Name_EN": "name_EN",
+                "Country_Name_FR": "name_FR",
+                "Country_Name_DE": "name_DE",
+            },
+            inplace=True,
+        )
+        self.data.dropna(
+            subset=[nameof(self.data.UIC_code)],
+            inplace=True,
         )
 
-    def _extract_info(self, country: _Element) -> tuple:
-        iso_code, name_en = self._extract_critical_info(country)
-        name_de, name_fr, uic_code = self._extract_extra_info(country)
-
-        return iso_code, uic_code, name_en, name_fr, name_de
-
-    def _extract_extra_info(self, country: _Element) -> tuple:
-        # TODO: remove line below when https://youtrack.jetbrains.com/issue/PY-16408/ is fixed
-        # noinspection PyUnusedLocal
-        uic_code = name_fr = name_de = None
-        with contextlib.suppress(AttributeError):
-            uic_code = int(self._find_value(country, "UIC_Code"))
-            name_fr = self._find_value(country, "Name_FR")
-            name_de = self._find_value(country, "Name_DE")
-        return name_de, name_fr, uic_code
-
-    def _extract_critical_info(self, country: _Element) -> tuple:
-        try:
-            iso_code = self._find_value(country, "ISO_Code")
-            name_en = self._find_value(country, "Name_EN")
-        except TypeError:
-            self.logger.critical(
-                f"Critical info could not be extracted from {nameof(self.data)}!"
+        columns_to_swap = [
+            nameof(self.data.name_EN),
+            nameof(self.data.name_FR),
+            nameof(self.data.name_DE),
+        ]
+        self.data[nameof(self.data.name_EN)] = self.data[
+            nameof(self.data.name_EN)
+        ].apply(lambda x: x.rstrip())
+        for column_name in columns_to_swap:
+            self.data[column_name] = self.data[column_name].apply(
+                lambda x: _swap_name(x)
             )
-            raise
-        return iso_code, name_en
 
-    def _find_value(self, row: _Element, column: str) -> str:
-        return row.find(self._TAG_BEGINNING_COLUMN + column, self.namespace).text  # type: ignore
+        with self.database.engine.begin() as connection:
+            for index, row in self.data.iterrows():
+                query = """
+                insert ignore into countries (
+                    ISO_code,
+                    UIC_code,
+                    name_EN,
+                    name_FR,
+                    name_DE
+                )
+                values (:ISO_code, :UIC_code, :name_EN, :name_FR, :name_DE)
+                """
+
+                connection.execute(
+                    text(query),
+                    {
+                        nameof(row.ISO_code): row.ISO_code,
+                        nameof(row.UIC_code): row.UIC_code,
+                        nameof(row.name_EN): row.name_EN,
+                        nameof(row.name_FR): row.name_FR,
+                        nameof(row.name_DE): row.name_DE,
+                    },
+                )
+
+        self.logger.info(
+            f"Successfully added new data downloaded from {self.DATA_URL} to table `countries`!"
+        )
