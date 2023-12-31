0f635c5399b6e283c71a34c66df14b3aa9b1726d
Máté Gyöngyösi <gyongyosimate1@gmail.com>
2023-12-31 23:52:41 +0100

    ⏬ Developed importing to DataFrame

--- src/process_new_email/table_updaters/companies.py
+++ src/process_new_email/table_updaters/companies.py
@@ -1,36 +1,53 @@
 from abc import ABC
+import re
 
-import pandas
+import pandas as pd
 
 from src.process_new_email.table_updaters.common import HelperTableUpdater
 
 
 class CompaniesUpdater(HelperTableUpdater, ABC):
-    def __init__(self, database_connection, data_url) -> None:
-        super().__init__(database_connection, data_url)
+    def __init__(self, database, data_url) -> None:
+        super().__init__(database, data_url)
 
-        self.data: pandas.DataFrame = pandas.DataFrame()
+        self.data: pd.DataFrame = pd.DataFrame()
 
         self.logger.info(f"{self.__class__.__name__} initialized!")
 
     def process_data(self) -> None:
-        self.data = pandas.read_excel(self._DATA_TO_PROCESS)
-        self.data.dropna(subset=["allocation date"], inplace=True)
+        # TODO: report bug to JetBrains
+        # noinspection PyTypeChecker
+        self.data = pd.read_excel(self._data_to_process)
+
+        # TODO: report wrong display of newlines in DataFrame view to pandas developers
+        self.data.rename(
+            columns=lambda x: re.sub(
+                r"[\n ]",
+                "_",
+                x,
+            ).lower(),
+            inplace=True,
+        )
+        self.data.rename(
+            columns={"infra-_structure": "infrastructure"},
+            inplace=True,
+        )
+
+        self.data.dropna(
+            subset=[
+                "allocation_date",
+                "infrastructure",
+            ],
+            inplace=True,
+        )
         self.data.drop(
             columns=[
-                "request date",
-                "begin of validity",
-                "end of validity",
+                "request_date",
                 "recent",
             ],
             inplace=True,
         )
+        pass
 
     def store_data(self) -> None:
-        self.data.to_sql(
-            name="companies",
-            self.database.engine,
-            if_exists="replace",
-            index=False,
-            method="multi",
-        )
+        pass
