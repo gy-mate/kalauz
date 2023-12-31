61308f9d3afb27dd1aa9f3970927d22a18ae08f7
Máté Gyöngyösi <gyongyosimate1@gmail.com>
2023-12-31 23:45:15 +0100

    🔄 Refactor main function

--- src/process_new_email/main.py
+++ src/process_new_email/main.py
@@ -2,7 +2,8 @@ import logging
 
 from dotenv import load_dotenv
 
-from src.process_new_email.database_connection import DatabaseServer
+from src.process_new_email.database_connection import Database
+
 from src.process_new_email.table_updaters.companies import CompaniesUpdater
 from src.process_new_email.table_updaters.country_codes import CountryCodesUpdater
 
@@ -10,25 +11,32 @@ from src.process_new_email.table_updaters.country_codes import CountryCodesUpdat
 def main() -> None:
     logging.basicConfig(
         encoding="utf-8",
-        handlers=[logging.StreamHandler(), logging.FileHandler("kalauz.log")],
-        format="%(asctime)s – %(levelname)s @ %(name)s @ %(funcName)s: %(message)s",
+        handlers=[
+            logging.StreamHandler(),
+            logging.FileHandler("kalauz.log"),
+        ],
+        format="%(asctime)s – %(levelname)s @ %(name)s.%(funcName)s(): %(message)s",
         level=logging.DEBUG,
     )
     load_dotenv()
 
-    database_connection = DatabaseServer(database_name="kalauz")
+    database_connection = Database(database_name="kalauz")
 
     update_country_codes(
-        database_connection, "https://uic.org/spip.php?action=telecharger&arg="
+        database_connection,
+        "https://uic.org/spip.php?action=telecharger&arg=",
     )
     update_companies(
-        database_connection, "https://uic.org/spip.php?action=telecharger&arg=3023"
+        database_connection,
+        "https://uic.org/spip.php?action=telecharger&arg=3023",
     )
 
 
 def update_country_codes(database_connection, uic_root_url: str) -> None:
     country_codes_updater = CountryCodesUpdater(
-        database_connection, f"{uic_root_url}322", f"{uic_root_url}320"
+        database_connection,
+        f"{uic_root_url}322",
+        f"{uic_root_url}320",
     )
 
     country_codes_updater.process_data()
