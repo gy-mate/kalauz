61205a566688c591d226f0609982810038d9b193
Máté Gyöngyösi <gyongyosimate1@gmail.com>
2024-02-09 23:28:22 +0100

    ➕ Implemented remaining simple attributes; reordered functions

--- src/process_new_email/SR_processors/MÁV.py
+++ src/process_new_email/SR_processors/MÁV.py
@@ -1,10 +1,13 @@
 from datetime import datetime
 import re
+from typing import final
 from zoneinfo import ZoneInfo
 
 from openpyxl.cell import Cell
 from pandas import DataFrame
-import roman
+
+# future: remove the comment below when stubs for the library below are available
+import roman  # type: ignore
 
 from src.process_new_email.SR_processors.common import SRUpdater
 from src.process_new_email.table_updaters.common import ExcelDeepProcessor
@@ -14,99 +17,38 @@ def _is_tsr(cell: Cell) -> bool:
     return cell.font.bold
 
 
-def _convert_cells_to_values(row: list[Cell]) -> list:
-    new_row: list = []
-    for cell in row:
-        row.append(cell)
-    return new_row
-
-
 def _header_or_footer(row: list) -> bool:
     return "Vonal" in str(row[0]) or "Összes korlátozás:" in str(row[1])
+
+
+def _get_reduced_speeds(text_to_search: str) -> tuple[int, int]:
+    if text_to_search.find("/") == -1:
+        reduced_speed = reduced_speed_for_mus = round(
+            int(re.findall(r".*(?= \()", text_to_search)[0])
+        )
+    else:
+        reduced_speed = round(int(re.findall(r".*(?=/)", text_to_search)[0]))
+        reduced_speed_for_mus = round(
+            int(re.findall(r"(?<=/).*(?= )", text_to_search)[0])
+        )
+    
+    return reduced_speed, reduced_speed_for_mus
+
+
+def _get_metre_post(text_to_search: str) -> int | None:
+    try:
+        assert isinstance(text_to_search, int)
+        return int(text_to_search * 100)
+    except AssertionError:
+        return None
 
 
 def remove_space_after_hyphen(data: str) -> str:
     return re.sub(r"(?<=\w)- (?=\w)", "-", str(data))
 
 
-def extract_number(data):
-    """
-    Receives a data.
-
-    Converts the data to a str.
-    Initializes *regex search expressions* for arabic and roman numbers with several combinations that could be found in the database.
-
-    Searches for an _arabic number._
-    If found, returns it as an int.
-    If not found, searches for a _roman number._
-
-    If found, converts it to an _arabic number_ via `fromroman` and returns it as an int.
-    If this doesn't succeed, returns an `InvalidRomanNumeralError`.
-    If not found, searches for an _arabic number with letters._
-
-    If found, returns it as an int.
-    If not found, returns a 'TypeError'.
-    """
-    arabic_regex = re.compile(
-        """
-        (
-        ^|(?<=[ .(])
-        )
-        
-        \\d+
-        
-        (?=\\D)
-        """,
-        re.VERBOSE | re.MULTILINE,
-    )
-
-    roman_mix_regex = re.compile(
-        """
-        (
-        ^|
-        (?<=[ .(])
-        )
-        
-        (
-        (M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3}))|
-        (M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3}))/[a-z]|
-        (M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3}))[a-z]
-        )
-        
-        (?=[ .])
-        """,
-        re.VERBOSE | re.MULTILINE,
-    )
-
-    arabic_letter_regex = re.compile(
-        """
-        (
-        ^|(?<=[ .(])
-        )
-        
-        \\w\\d+
-        (?=\\D)
-        """,
-        re.VERBOSE | re.MULTILINE,
-    )
-
-    text = str(data)
-
-    try:
-        if arabic_regex.search(text) is not None:
-            return int(arabic_regex.search(text)[0])
-        else:
-            if roman_mix_regex.search(text) is not None:
-                if roman_mix_regex.search(text)[0]:
-                    return int(roman.fromRoman(roman_mix_regex.search(text)[0]))
-                else:
-                    return arabic_letter_regex.search(text)[0]
-            else:
-                return arabic_letter_regex.search(text)[0]
-    except TypeError:
-        return "TypeError"
-    except roman.InvalidRomanNumeralError:
-        return "InvalidRomanNumeralError"
+def _get_operating_speed(text_to_search: str) -> int:
+    return _get_number_between_brackets(text_to_search)
 
 
 def _get_number_between_brackets(text_to_search: str) -> int:
@@ -121,32 +63,7 @@ def _get_end_time(text_to_search: str) -> str:
     return text_to_search[12:22]
 
 
-def _get_metre_post(text_to_search: str) -> int | None:
-    try:
-        assert isinstance(text_to_search, int)
-        return int(text_to_search * 100)
-    except AssertionError:
-        return None
-
-
-def _get_reduced_speeds(text_to_search: str) -> tuple[int, int]:
-    if text_to_search.find("/") == -1:
-        reduced_speed = reduced_speed_for_mus = round(
-            int(re.findall(r".*(?= \()", text_to_search)[0])
-        )
-    else:
-        reduced_speed = round(int(re.findall(r".*(?=/)", text_to_search)[0]))
-        reduced_speed_for_mus = round(
-            int(re.findall(r"(?<=/).*(?= )", text_to_search)[0])
-        )
-
-    return reduced_speed, reduced_speed_for_mus
-
-
-def _get_operating_speed(text_to_search: str) -> int:
-    return _get_number_between_brackets(text_to_search)
-
-
+@final
 class MavUpdater(SRUpdater, ExcelDeepProcessor):
     def __init__(self) -> None:
         super().__init__(
@@ -161,25 +78,27 @@ class MavUpdater(SRUpdater, ExcelDeepProcessor):
         self.data = DataFrame(columns=columns)
         for worksheet in self._data_to_process:
             for row_of_cells in [list(cell) for cell in worksheet.iter_rows()]:
-                is_temporary = _is_tsr(row_of_cells[0])
+                in_timetable = not _is_tsr(row_of_cells[0])
                 row = [str(cell.value) for cell in row_of_cells]
 
                 if not _header_or_footer(row):
                     reduced_speed, reduced_speed_for_mus = _get_reduced_speeds(row[8])
+                    
+                    metre_post_to = _get_metre_post(row[6])
 
                     row_to_add = {
                         "country_iso": self.COUNTRY_CODE_ISO,
                         "company_uic": self.COMPANY_CODE_UIC,
                         "internal_id": None,
-                        "in_timetable": not is_temporary,
+                        "in_timetable": in_timetable,
                         "due_to_railway_features": NotImplemented,
                         "line": row[0],
                         "metre_post_from": _get_metre_post(row[5]),
-                        "metre_post_to": _get_metre_post(row[6]),
-                        "open_line_tracks_left_or_right": NotImplemented,
+                        "metre_post_to": metre_post_to,
                         "station_from": remove_space_after_hyphen(row[1]),
-                        "station_to": NotImplemented,
-                        "on_main_track": NotImplemented,
+                        "station_to": remove_space_after_hyphen(row[2]) if row[2] else None,
+                        "on_main_track": True if row[3] else False,
+                        "main_track_left_or_right": self._on_right_track(row[3]),
                         "station_track_switch_source_text": row[4],
                         "station_track_switch_from": NotImplemented,
                         "station_track_switch_to": NotImplemented,
@@ -189,40 +108,26 @@ class MavUpdater(SRUpdater, ExcelDeepProcessor):
                         "cause": row[12],
                         "date_from": _get_date(row[11]),
                         "time_from": self._convert_date_to_iso(row[11]),
-                        "maintenance_planned": NotImplemented,
-                        "date_to": NotImplemented,
+                        "maintenance_planned": None,
+                        "date_to": _get_date(row[13]) if row[13] else None,
                         "time_to": (
                             self._convert_date_to_iso(row[13]) if row[13] else None
                         ),
-                        "work_to_be_done": NotImplemented,
+                        "work_to_be_done": None,
                         "comment": row[14],
                     }
-
-                    if current_sr.on_station:
-                        if "kitérő" in str(row[4]):
-                            current_sr.applied_to = "switch"
-                            current_sr.switch_station = extract_number(row[4])
-                        else:
-                            current_sr.applied_to = "track"
-                            current_sr.track_station = extract_number(row[4])
-                    else:
-                        station_to_name = remove_space_after_hyphen(row[2])
-                        current_sr.station_to_uic = get_station_uic(
-                            current_sr.country_uic, station_to_name
-                        )
-                        current_sr.applied_to = "track"
-                        if row[3] is None:
-                            current_sr.number_of_open_line_tracks = 1
-                        elif row[3] == "bal vágány":
-                            current_sr.number_of_open_line_tracks = 2
-                            current_sr.track_open_line = "left"
-                        elif row[3] == "jobb vágány":
-                            current_sr.number_of_open_line_tracks = 2
-                            current_sr.track_open_line = "right"
-                        else:
-                            current_sr.number_of_open_line_tracks = (
-                                current_sr.track_open_line
-                            ) = "UnknownError"
+                    
+    def _on_right_track(self, text_to_search: str) -> bool:
+        try:
+            if "bal" in text_to_search:
+                return False
+            elif "jobb" in text_to_search:
+                return True
+            else:
+                raise ValueError(f"Unrecognized track side: {text_to_search}!")
+        except ValueError as exception:
+            self.logger.critical(exception)
+            raise
 
     def _convert_date_to_iso(self, text_to_search: str) -> datetime:
         try:
