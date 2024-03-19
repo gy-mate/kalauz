92e01d3e2b7637c4253c57d97c0db3d6ffd35b80
Máté Gyöngyösi <gyongyosimate1@gmail.com>
2024-03-19 18:56:54 +0100

    👨‍👩‍👧‍👦 Added inheritance of `ExcelProcessorSimple`

--- src/new_data_processors/SR_processors/companies/GYSEV.py
+++ src/new_data_processors/SR_processors/companies/GYSEV.py
@@ -1,33 +1,22 @@
 from typing import final
 
 from src.new_data_processors.SR_processors.common import SRUpdater
+from src.new_data_processors.common_excel_processors import ExcelProcessorSimple
 
 
 @final
-class GysevUpdater(SRUpdater):
+class GysevUpdater(SRUpdater, ExcelProcessorSimple):
     def __init__(self) -> None:
         super().__init__(
             company="GYSEV",
             source_extension="xlsx",
         )
-
-    def _get_data(self) -> None:
-        pass
-
-    def _import_data(self) -> None:
-        pass
-
-    def _correct_data_manually(self) -> None:
-        pass
-
+        
     def _rename_columns_manually(self) -> None:
         pass
-
+    
     def _correct_boolean_values(self) -> None:
         pass
-
-    def _create_table_if_not_exists(self) -> None:
-        pass
-
+    
     def _add_data(self) -> None:
         pass
