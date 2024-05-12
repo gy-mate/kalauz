328808c9b3cdb82fbc7de58e59a1691390ba788e
Máté Gyöngyösi <gyongyosimate1@gmail.com>
2024-05-12 20:37:18 +0200

    ⛔️ Handled dead ends; normalized node (coordinate) orders; added further (percentage) logging

--- src/OSM_processors/mapper.py
+++ src/OSM_processors/mapper.py
@@ -185,13 +185,12 @@ def get_ways_between_milestones(
     way_of_greater_milestone: Way,
     way_of_lower_milestone: Way,
     ways_of_line: list[Way],
-    sr: SR,
 ) -> list[Way]:
     if way_of_lower_milestone is way_of_greater_milestone:
         return [way_of_lower_milestone]
 
     ways_of_line_copy = ways_of_line.copy()
-    ways_of_line.remove(way_of_lower_milestone)
+    ways_of_line_copy.remove(way_of_lower_milestone)
     neighbouring_ways_of_lower_milestone: tuple[list[Way], list[Way]] = (
         [way_of_lower_milestone],
         [way_of_lower_milestone],
@@ -200,7 +199,6 @@ def get_ways_between_milestones(
     toggle = False
     return add_neighboring_ways(
         collection=neighbouring_ways_of_lower_milestone,
-        source_way=way_of_lower_milestone,
         ways_to_search_in=ways_of_line_copy,
         destination_way=way_of_greater_milestone,
         toggle=toggle,
@@ -208,46 +206,91 @@ def get_ways_between_milestones(
 
 
 def add_neighboring_ways(
-    collection: tuple[list[Way], list[Way]], source_way: Way, ways_to_search_in: list[Way], destination_way: Way, toggle: bool
+    collection: tuple[list[Way], list[Way]],
+    ways_to_search_in: list[Way],
+    destination_way: Way,
+    toggle: bool,
+    one_side_is_dead_end: bool = False,
 ) -> list[Way]:
     for way in ways_to_search_in:
-        found_neighbor_way = (source_way.nodes[0] in way.nodes) or (
-            source_way.nodes[-1] in way.nodes
+        found_neighbor_way = (collection[toggle][-1].nodes[0] in way.nodes) or (
+            collection[toggle][-1].nodes[-1] in way.nodes
         )
         if found_neighbor_way:
             collection[toggle].append(way)
             if way is destination_way:
                 return collection[toggle]
             ways_to_search_in.remove(way)
-            ways_to_search_in.reverse()
-            toggle = not toggle
-            add_neighboring_ways(
+            if not one_side_is_dead_end:
+                ways_to_search_in.reverse()
+                toggle = not toggle
+            return add_neighboring_ways(
                 collection,
-                source_way,
                 ways_to_search_in,
                 destination_way,
                 toggle,
             )
+    if one_side_is_dead_end:
+        raise ValueError("Couldn't reach destination_way from way_of_lower_milestone!")
+    else:
+        one_side_is_dead_end = True
+        ways_to_search_in.reverse()
+        toggle = not toggle
+        return add_neighboring_ways(
+            collection,
+            ways_to_search_in,
+            destination_way,
+            toggle,
+            one_side_is_dead_end,
+        )
 
 
 def merge_ways(ways_between_milestones: list[Way]) -> shapely.LineString:
     coordinates: list[tuple[float, float]] = []
     for way in ways_between_milestones:
         way_coordinates = [(float(node.lon), float(node.lat)) for node in way.nodes]
+        if coordinates:
+            fix_misaligned_list_orders(coordinates, way_coordinates)
         coordinates.extend(way_coordinates)
     merged_ways_between_milestones = shapely.LineString(coordinates)
     return merged_ways_between_milestones
 
 
+def fix_misaligned_list_orders(
+    coordinates: list[tuple[float, float]], way_coordinates: list[tuple[float, float]]
+) -> None:
+    for existing_coordinate_id in 0, -1:
+        for way_coordinate_id in 0, -1:
+            found_matching_coordinates = (
+                coordinates[existing_coordinate_id]
+                == way_coordinates[way_coordinate_id]
+            )
+            if found_matching_coordinates:
+                if existing_coordinate_id == 0:
+                    coordinates.reverse()
+                if way_coordinate_id == -1:
+                    way_coordinates.reverse()
+                return
+
+
 def split_lines(
-    merged_ways_between_milestones: shapely.LineString,
+    line: shapely.LineString,
     splitting_point: shapely.Point,
 ) -> shapely.MultiLineString:
-    split_lines_at_lower_milestone = split(
-        geom=merged_ways_between_milestones,
-        splitter=splitting_point,
-    )
-    return split_lines_at_lower_milestone
+    # future: debug why it gives "RuntimeWarning: invalid value encountered in line_locate_point
+    #   return lib.line_locate_point(line, other)" at ./venv/lib/python3.12/site-packages/shapely/linear.py:88 when
+    #   line.wkt is LINESTRING (19.1090192 47.4882755, 19.1092076 47.4882598, 19.1096989 47.488219, 19.1100772 47.4882149, 19.1104284 47.4882206, 19.1108077 47.4882435, 19.1112823 47.4882943, 19.1130505 47.4885548, 19.1136396 47.4885698, 19.1142464 47.4885517, 19.1148753 47.4884826, 19.1156548 47.4883176, 19.1161264 47.4881959, 19.1167118 47.4880177, 19.1171248 47.4878589, 19.1174105 47.4877445, 19.1175931 47.487661, 19.117763 47.4875706, 19.1178895 47.4874906, 19.1180338 47.4873916, 19.1180521 47.4873774, 19.1181963 47.4872654, 19.118378 47.4871053, 19.1185497 47.4869549, 19.1186807 47.4868195, 19.118776 47.4867018, 19.1188674 47.4865653, 19.1189273 47.4864621, 19.1190071 47.4863118, 19.1190717 47.4861743, 19.1191184 47.4860293, 19.1191634 47.4858775, 19.1191815 47.4857592, 19.1191993 47.4854503, 19.1191915 47.4851474, 19.1189492 47.4836573, 19.1189492 47.4836573, 19.118906 47.4833618, 19.118906 47.4833618, 19.1185928 47.4815841, 19.1184233 47.4809331, 19.118266 47.480416, 19.1178267 47.4795529, 19.1178267 47.4795529, 19.117767 47.4794579, 19.1177241 47.4793908, 19.1176786 47.4793254, 19.117484 47.4790628, 19.1174267 47.4789866, 19.1173673 47.4789118, 19.1172806 47.478805, 19.1171216 47.4786218, 19.1170269 47.4785158, 19.1169512 47.4784288, 19.1168806 47.478349, 19.116788 47.478246, 19.1167084 47.4781616, 19.1166264 47.4780777, 19.116551 47.4779976, 19.1164002 47.4778344, 19.1164002 47.4778344, 19.1163115 47.4777367, 19.1163115 47.4777367, 19.1159898 47.4773938, 19.1159099 47.4773088, 19.115792 47.4771889, 19.1156595 47.4770514, 19.1156269 47.4770205, 19.1152604 47.4766626, 19.1151741 47.476578, 19.1150855 47.4764934, 19.1149967 47.4764095, 19.1149044 47.4763264, 19.1146304 47.476095, 19.1146304 47.476095, 19.1144545 47.475952, 19.1143893 47.4758978, 19.1143008 47.4758301, 19.1142193 47.4757687, 19.1141395 47.4757097, 19.114063 47.4756577, 19.113604 47.47537, 19.1134839 47.4752959, 19.11336 47.4752207, 19.113235 47.4751466, 19.1131084 47.4750743, 19.112914 47.4749619, 19.1127883 47.4748892, 19.1126575 47.4748164, 19.1125435 47.474755, 19.1124268 47.474695, 19.1123053 47.474633, 19.1121836 47.4745731, 19.1114544 47.4742379, 19.1105156 47.4738509, 19.1095889 47.473512, 19.1069261 47.4726811, 19.1069261 47.4726811, 19.1064554 47.4725344, 19.1064554 47.4725344, 19.1062799 47.4724791, 19.106091 47.4724221, 19.1058994 47.4723659, 19.105706 47.4723112, 19.1055111 47.4722584, 19.1053648 47.4722184, 19.1052122 47.4721805, 19.1049803 47.472126, 19.1047393 47.4720709, 19.104497 47.4720199, 19.1042904 47.4719777, 19.104059 47.4719291, 19.1038323 47.4718797, 19.103596 47.4718255, 19.1033932 47.4717758, 19.1031971 47.4717261, 19.1028049 47.4716232, 19.1013903 47.4712538, 19.1013903 47.4712538, 19.1003118 47.4709719, 19.1003118 47.4709719, 19.0994393 47.4707451, 19.0980992 47.4703949, 19.0942493 47.4693886, 19.0940057 47.4693284, 19.0937514 47.4692701, 19.0934971 47.4692157, 19.0932394 47.4691652, 19.0929873 47.4691185, 19.0927312 47.4690752, 19.0924436 47.469028, 19.0922123 47.4689925, 19.0903963 47.4687161, 19.0902082 47.4686927, 19.0900073 47.4686674, 19.0898052 47.4686444, 19.0895985 47.4686215, 19.0894278 47.4686055, 19.089353 47.4685985, 19.0891311 47.4685803, 19.0889092 47.4685649, 19.088692 47.4685522, 19.088266 47.4685298, 19.0879508 47.4685128, 19.0871293 47.4684745)
+    #   and splitting_point.wkt is POINT (19.1180521 47.4873774)
+
+    # future: remove supression below when the issue above is fixed
+
+    # future: report bug (false positive) to JetBrains developers
+    # noinspection PyUnboundLocalVariable
+    with contextlib.suppress(RuntimeWarning):
+        return split(
+            geom=line,
+            splitter=splitting_point,
+        )
 
 
 class Mapper(DataProcessor):
@@ -266,14 +309,15 @@ class Mapper(DataProcessor):
         """
         self.OPERATORS: Final = ["MÁV", "GYSEV"]
         self.OPERATING_SITE_TAG_VALUES: Final = [
-            "station",
-            "halt",
-            "yard",
-            "service_station",
-            "junction",
-            "crossover",
-            "spur_junction",
-            "site",
+            # TODO: uncomment lines below when implementing stations
+            # "station",
+            # "halt",
+            # "yard",
+            # "service_station",
+            # "junction",
+            # "crossover",
+            # "spur_junction",
+            # "site",
         ]
 
         self._api: Final = Overpass()
@@ -548,13 +592,21 @@ class Mapper(DataProcessor):
         self.add_all_nodes(features_to_visualise)
         self.add_na_lines(features_to_visualise)
 
-        for sr in self.srs:
+        self.logger.info(f"Visualising {len(self.srs)} speed restrictions started...")
+        notification_percentage_interval = 1
+        notify_at_indexes: list[int] = []
+        for i in range(1, int(100 / notification_percentage_interval) - 1):
+            notify_at_index = int(
+                len(self.srs) * (notification_percentage_interval / 100) * i
+            )
+            notify_at_indexes.append(notify_at_index)
+        for sr_index, sr in enumerate(self.srs):
             try:
                 ways_of_line = self.get_ways_of_corresponding_line(sr)
                 nodes_of_line = [node for way in ways_of_line for node in way.nodes]
                 milestones_of_line = get_milestones(nodes=nodes_of_line)
 
-                for i, sr_metre_post_boundary in enumerate(
+                for j, sr_metre_post_boundary in enumerate(
                     (sr.metre_post_from, sr.metre_post_to)
                 ):
                     nearest_milestones: list[Node] = []
@@ -578,12 +630,11 @@ class Mapper(DataProcessor):
                         way_of_greater_milestone,
                         way_of_lower_milestone,
                         ways_of_line,
-                        sr,
                     )
                     merged_ways_between_milestones = merge_ways(ways_between_milestones)
 
                     split_lines_at_lower_milestone = split_lines(
-                        merged_ways_between_milestones=merged_ways_between_milestones,
+                        line=merged_ways_between_milestones,
                         splitting_point=shapely.Point(
                             (
                                 float(nearest_milestones[0].lon),
@@ -592,7 +643,7 @@ class Mapper(DataProcessor):
                         ),
                     )
                     split_lines_at_greater_milestone = split_lines(
-                        merged_ways_between_milestones=merged_ways_between_milestones,
+                        line=merged_ways_between_milestones,
                         splitting_point=shapely.Point(
                             (
                                 float(nearest_milestones[-1].lon),
@@ -600,16 +651,10 @@ class Mapper(DataProcessor):
                             )
                         ),
                     )
-                    if sr.main_track_side == "left":
-                        line_between_milestones = shapely.intersection(
-                            split_lines_at_lower_milestone.geoms[0],
-                            split_lines_at_greater_milestone.geoms[1],
-                        )
-                    else:
-                        line_between_milestones = shapely.intersection(
-                            split_lines_at_lower_milestone.geoms[1],
-                            split_lines_at_greater_milestone.geoms[0],
-                        )
+                    line_between_milestones = shapely.intersection(
+                        split_lines_at_lower_milestone.geoms[1],
+                        split_lines_at_greater_milestone.geoms[0],
+                    )
 
                     coordinate_of_metre_post = line_between_milestones.interpolate(
                         distance=metre_post_at_percentage_between_milestones,
@@ -641,7 +686,10 @@ class Mapper(DataProcessor):
                 else:
                     self.logger.debug(exception)
                     raise
+            if sr_index in notify_at_indexes:
+                self.logger.info(f"{int(sr_index / len(self.srs) * 100)}% done")
             pass
+        self.logger.info(f"...finished visualising speed restrictions!")
 
         feature_collection_to_visualise = geojson.FeatureCollection(
             features_to_visualise
@@ -655,6 +703,7 @@ class Mapper(DataProcessor):
         return ways
 
     def add_na_lines(self, features_to_visualise: list[geojson.Feature]) -> None:
+        self.logger.debug(f"Adding N/A lines started...")
         for way in self.osm_data.ways:
             way_line = geojson.LineString(
                 [(float(node.lon), float(node.lat)) for node in way.nodes]
@@ -670,8 +719,10 @@ class Mapper(DataProcessor):
                 properties=way.tags,
             )
             features_to_visualise.append(feature)
+        self.logger.debug(f"...finished!")
 
     def add_all_nodes(self, features_to_visualise: list[geojson.Feature]) -> None:
+        self.logger.debug(f"Adding all nodes started...")
         for node in self.osm_data.nodes:
             if node.id != 1:
                 point = geojson.Point((float(node.lon), float(node.lat)))
@@ -682,6 +733,7 @@ class Mapper(DataProcessor):
                     properties=node.tags,
                 )
                 features_to_visualise.append(feature)
+        self.logger.debug(f"...finished!")
 
     def export_map(self, feature_collection: geojson.FeatureCollection) -> None:
         geojson_layer = Layer(
