299e314a6b64870c847d06bcbe9c07052b4d26a4
Máté Gyöngyösi <gyongyosimate1@gmail.com>
2024-06-28 23:04:36 +0200

    ➖ Removed total slide count from footer

--- presentation/global-top.vue
+++ presentation/global-top.vue
@@ -2,7 +2,7 @@
   <footer class="absolute bottom-0 left-0 right-0 p-2"
           style="background: rgba(255, 255, 255, 0.7); display: flex; justify-content: center">
     <div>
-      <SlideCurrentNo /> / <SlidesTotal />
+      <SlideCurrentNo/>
     </div>
   </footer>
 </template>
